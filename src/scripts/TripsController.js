621e5c7b2852edacfe9d9de32e12585ac3cd68a5
Frederick Roegiers <frederick@rogerthat.be>
2020-03-19 15:38:28 +0100

    wip

--- src/scripts/TripsController.js
+++ src/scripts/TripsController.js
@@ -4,13 +4,16 @@ class TripsController {
     constructor(template_wrapper) {
         this.wrapper = template_wrapper;
     }
+
     home() {
         let template = nunjucks.render('pages/home.html');
         this.wrapper.innerHTML = template;
     }
+
     about() {
         
     }
+    
     contact() {
 
     }
